837fac46c66259d6973849822e9b2aa6f3577f5d
Brandon Green <green.finite@gmail.com>
2023-11-10 15:44:56 -0800

    Add code to execute DB transaction, committing csv seller data to DB

--- util/copy_sellers_to_db.py
+++ util/copy_sellers_to_db.py
@@ -1,4 +1,5 @@
 import psycopg2
+import psycopg2.extras
 from typing import List, Tuple
 
 def connect_to_db(db_params):
@@ -11,26 +12,35 @@ def connect_to_db(db_params):
     print("Connection successful")
     return conn
 
-def copy_to_db(conn):
-    source_file = "data/three_sellers.txt"
-    # Create cursor to execute queries
-    db_cursor = conn.cursor()
+def copy_to_db(conn, source_file):
+    """
+    Commit CSV values to the card shops database.
+    """
     # TODO, we thought we couldn't use copy_from because of the UUID column
     # and the fact that the csv file does not have UUIDs inside.
     # however, using a UUID seems overkill.
     # We can use each shop's hyperlink as a unique identifier.
-    # We should do some file processing to insert the hyperlink at the start of
-    # every line in the csv file
+    RATING_IDX = 3
+    SALES_IDX = 4
+    STATE_IDX = 5
+    field_names = [
+        'db_id', 'shop_name', 'hyperlink', 'rating', 'num_sales', 'state'
+    ]
+    # create list of dictionary objects for each entry in the csv file
+    csv_data = []
     with open(source_file) as f:
         for line in f:
-            line_parts = line.split(',')
-            shop_link = line_parts[1]
-            # Find the '/' preceding the unique link section and slice
-            target_index = shop_link.rfind('/')
-            target = shop_link[target_index + 1:]
-        
-    conn.commit()
-    db_cursor.close()
+            sl = line.split(',')
+            sl[RATING_IDX] = float(sl[RATING_IDX])
+            sl[SALES_IDX] = int(sl[SALES_IDX])
+            sl[STATE_IDX] = sl[STATE_IDX][:-1] # Remove the newline character
+            csv_data.append(dict(zip(field_names, sl)))
+    # Create cursor to execute queries. context manager handles committing and closing cursor
+    with conn.cursor() as db_cursor:
+        insert_query = """INSERT INTO card_shops (db_id, shop_name, hyperlink, rating, num_sales, state) VALUES (%(db_id)s, %(shop_name)s, %(hyperlink)s, %(rating)s, %(num_sales)s, %(state)s);"""
+        psycopg2.extras.execute_batch(db_cursor, insert_query, csv_data)
+        conn.commit()
+    print("Exited psql context manager")
 
 def prepare_data_for_insertion(filename) -> List[Tuple]:
     """
@@ -60,14 +70,16 @@ def test_helper():
 def main():
     db_conn_params = {
         "host"  : "localhost",
-        "dbname": "mtg_info",
-        "user"  : "brandon"
+        "dbname": "tcg_shops",
+        "user"  : "postgres"
     }
+    # source_file = "data/three_sellers.csv"
+    source_file = "data/sellers_Tue_05_Sep_2023_02_04AM_with_key_new.csv"
 
     db_conn = connect_to_db(db_conn_params)
-    copy_to_db(db_conn)
+    copy_to_db(db_conn, source_file)
     db_conn.close()
 
 if __name__ == '__main__':
-    #main()
-    test_helper()
+    main()
+    # test_helper()
